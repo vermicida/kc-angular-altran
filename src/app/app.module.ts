0b491f7f83d25d977838a1d671857cded304b84e
Diego Herrera <vermicida@gmail.com>
2017-11-13 22:29:20 +0100

    Repasamos rápidamente el módulo AppModule de nuestra agenda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
+  // En el metadato 'declarations' indicamos todos los componentes, directivas y
+  // pipes de nuestra app. Si no los añadimos, nuestra app (módulo) no sabrá
+  // cómo pintarlos en pantalla o cómo utilizarlos.
   declarations: [
     AppComponent
   ],
+  // En el metadato 'imports' indicamos todos aquellos módulos de los cuáles depende
+  // nuestra app para funcionar.
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  // En el metadato 'providers' añadimos los proveedores de aquellas servicios, clases
+  // y valores que quieran inyectarse como dependencias en alguna pieza de nuestra app.
+  // De no hacerlo, Angular no sabrá cómo instanciar aquella dependencia que le estamos
+  // pidiendo.
   providers: [],
-  bootstrap: [AppComponent]
+  // En el metadato 'bootstrap' establecemos el componente raíz de nuestra app.
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
